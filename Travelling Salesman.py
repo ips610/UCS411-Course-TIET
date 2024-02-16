1bb854abdc54c4a2270eec05ada1dd60a410f072
Ishpuneet Singh <ishpuneetsingh6@gmail.com>
2024-02-16 15:08:29 +0530

    Update Travelling Salesman.py

--- Travelling Salesman.py
+++ Travelling Salesman.py
@@ -2,29 +2,27 @@ import sys
 import itertools
 V = 4
 
+def TSP(graph, source):
+    vertex = []
+    for i in range(V):
+        if i!=source :
+            vertex.append(i)
+            
+    min_path = sys.maxsize
+    next_permutation = list(itertools.permutations(vertex))
+    print(next_permutation)
+    
+    for i in next_permutation:
+        current_pathweight = 0
+        k = source
+        for j in i:
+            current_pathweight += graph[k][j]
+            k = j
+        current_pathweight += graph[k][source]
+        min_path = min(min_path, current_pathweight)
+        
+    return min_path
 
-def travellingSalesmanProblem(graph, s): 
-
-	vertex = [] 
-	for i in range(V): 
-		if i != s: 
-			vertex.append(i) 
-
-	min_path = sys.maxsize 
-	next_permutation=itertools.permutations(vertex)
-	for i in next_permutation:
-
-		current_pathweight = 0
-
-		k = s 
-		for j in i: 
-			current_pathweight += graph[k][j] 
-			k = j 
-		current_pathweight += graph[k][s] 
-
-		min_path = min(min_path, current_pathweight) 
-		
-	return min_path 
 
 
 # Driver Code 
@@ -33,5 +31,5 @@ if __name__ == "__main__":
 	# matrix representation of graph 
 	graph = [[0, 10, 15, 20], [10, 0, 35, 25], 
 			[15, 35, 0, 30], [20, 25, 30, 0]] 
-	s = int(input("Enter Source Node: "))
-	print(travellingSalesmanProblem(graph, s))
+	source = int(input("Enter Source Node: "))
+	print(TSP(graph, source - 1))
