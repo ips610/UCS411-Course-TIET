ae8ce8b5f21373abbb76d238c829e6ff53ca9d39
Ishpuneet Singh <ishpuneetsingh6@gmail.com>
2024-02-16 09:09:15 +0530

    Create Travelling Salesman.py

---
+++ Travelling Salesman.py
@@ -0,0 +1,37 @@
+import sys
+import itertools
+V = 4
+
+
+def travellingSalesmanProblem(graph, s): 
+
+	vertex = [] 
+	for i in range(V): 
+		if i != s: 
+			vertex.append(i) 
+
+	min_path = sys.maxsize 
+	next_permutation=itertools.permutations(vertex)
+	for i in next_permutation:
+
+		current_pathweight = 0
+
+		k = s 
+		for j in i: 
+			current_pathweight += graph[k][j] 
+			k = j 
+		current_pathweight += graph[k][s] 
+
+		min_path = min(min_path, current_pathweight) 
+		
+	return min_path 
+
+
+# Driver Code 
+if __name__ == "__main__": 
+
+	# matrix representation of graph 
+	graph = [[0, 10, 15, 20], [10, 0, 35, 25], 
+			[15, 35, 0, 30], [20, 25, 30, 0]] 
+	s = 0
+	print(travellingSalesmanProblem(graph, s))
